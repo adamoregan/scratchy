a11d868a4c52e9fc5c991badbc2ca746acecde52
adamoregan <adamoregan457@gmail.com>
2024-09-26 22:41:29 +0100

    Add soup.py. Add support for limit on number of internal links scraped

--- scratchy.py
+++ scratchy.py
@@ -1,8 +1,7 @@
 import bs4
 import requests
-import urlutil as ut
-import urllib.parse as u
 from collections import deque
+from soup import get_internal_urls
 
 try:
     from lxml import etree
@@ -10,41 +9,47 @@ try:
 except ImportError:
     PARSER = 'html.parser'
 
-# SITEMAP_EXTS = (".txt", ".xml")
-# TODO: Implement generate_sitemap function
+__all__ = ['internal_scrape']
+
+def _internal_scrape(start_url: str, url_queue: deque, visited_urls: set):
+    """
+    Gets all the internal links in a webpage.
+
+    :param start_url: The url whos domain will be compared to the other links scraped.
+    :param url_queue: The queue of urls that need to be scraped.
+    :param visited_urls: The set of urls that have already been visited.
+    """
+    cur_url = url_queue.popleft()
+    try:
+        res = requests.get(cur_url)
+        res.raise_for_status()
+    except Exception as e:
+        print(f"! Error: {e}")
+        visited_urls.add(cur_url)
+        return
+    visited_urls.add(cur_url)
+    soup = bs4.BeautifulSoup(res.text, PARSER)
+    print(f"Scraping: {cur_url}")
+    urls = get_internal_urls(start_url, soup)
+    for url in urls:
+        if (url not in visited_urls) and (url not in url_queue):
+            # Add the url to the queue
+            url_queue.append(url)
 
 
-def internal_scrape(start_url: str) -> set:
+def internal_scrape(start_url: str, max_urls: int = None) -> set:
     """
-    Gets all the internal links in a website
-    :param start_url: The url to start scraping from
-    :return: A set of all the internal links in the website
+    Gets all the internal links in a website.
+    :param start_url: The url to start scraping from.
+    :param max_urls: The max number of urls to visit. If unspecified, no limit is set.
+    :return: A set of all the internal links in the website.
     """
-    parsed_start = u.urlparse(start_url)
     url_queue = deque([start_url])
     visited_urls = set()
-    while len(url_queue) != 0:
-        cur_url = url_queue.popleft()
-        try:
-            res = requests.get(cur_url)
-            res.raise_for_status()
-        except Exception as e:
-            print(f"! Error: {e}")
-            visited_urls.add(cur_url)
-            continue
-        visited_urls.add(cur_url)
-        soup = bs4.BeautifulSoup(res.text, PARSER)
-        print(f"Scraping: {cur_url}")
-        # Get all links on a page
-        for a in soup.find_all("a", href=True):
-            # Create an absolute url from a relative url
-            cur_internal_url = u.urljoin(cur_url, a['href'])
-            cur_internal_url = ut.remove_fragment(cur_internal_url)
-            parsed_cur_internal = u.urlparse(cur_internal_url)
-            # Check if it is an internal link
-            if ut.is_internal_parsed(parsed_start, parsed_cur_internal):
-                # If the url is not already in the queue and not in the visited links
-                if (cur_internal_url not in url_queue) and (cur_internal_url not in visited_urls):
-                    # Add the url to the queue
-                    url_queue.append(cur_internal_url)
+    if max_urls is None:
+        while len(url_queue) != 0:
+            _internal_scrape(start_url, url_queue, visited_urls)
+    else:
+        while len(url_queue) != 0 and len(visited_urls) < max_urls:
+            _internal_scrape(start_url, url_queue, visited_urls)
     return visited_urls
---
+++ soup.py
@@ -0,0 +1,24 @@
+import bs4
+import urlutil as ut
+import urllib.parse as u
+
+def get_internal_urls(url: str, soup: bs4.BeautifulSoup) -> set:
+    """
+    Gets all of the internal links in a soup object.
+
+    :param url: The url whos domain is being compared to the links scraped.
+    :param soup: The BeautifulSoup that is being parsed to get the internal links.
+    :return: A set of all unique internal links in the soup object.
+    """
+    urls = set()
+    parsed_link = u.urlparse(url)
+    # Get all links on a page
+    for a in soup.find_all("a", href=True):
+        # Create an absolute url from a relative url
+        cur_internal_url = u.urljoin(url, a['href'])
+        cur_internal_url = ut.remove_fragment(cur_internal_url)
+        parsed_cur_internal = u.urlparse(cur_internal_url)
+        # Check if it is an internal link
+        if ut.is_internal_parsed(parsed_link, parsed_cur_internal):
+            urls.add(cur_internal_url)
+    return urls
